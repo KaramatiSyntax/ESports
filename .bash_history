6e83d92b66bfe467dee56b6577e265ef2c961aea
KaramatiSyntax <manasshukla205@gmail.com>
2025-01-27 12:06:28 +0530

    all added

---
+++ .bash_history
@@ -0,0 +1,34 @@
+npm init -y
+cd "/sdcard/Code/Project/Rock Paper scissor"
+npm init -y
+cd "/sdcard/Code/Project/Rock Paper scissor"
+nodemon index
+nodemon
+nodemon index.js
+main
+nodemon /homecd "$HOME"
+npm i express
+npm i -g nodemon
+index.js
+/index.js
+node index
+nodemon index
+node main
+node index
+nodemon main
+nodemon index
+nodemon main
+nodemon index
+nodemon vinayak
+nodemon index
+nodemon vinayak
+nodemon index.js
+nodemon vinayak
+nodemon index.js
+acodeX-server
+npm i cors
+nodemon index.js
+acodeX-server
+AcodeX-server
+acodeX-server
+nodemon index.js
---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require("express")
+const cors = require('cors');
+const fs = require("fs");
+
+const app = express()
+const port = 3000
+
+app.use(cors());
+app.use(express.json());;
+
+app.get('/', (req, res) => {
+  if(req.query.user == undefined || req.query.id == undefined){
+    res.sendFile('signup.html', {root: __dirname})
+  }
+  else{
+    res.sendFile('Lndex.html', {root: __dirname});
+  }
+})
+let data = fs.readFileSync("./user.json");
+let myObject = JSON.parse(data);
+
+// Function to check if a user exists
+function userExists(username) {
+    return myObject.some(user => user.username == username);
+}
+
+app.post('/signup', (req, res) => {
+    const inputValue = req.body
+    const usernameToCheck = inputValue.username;
+
+    if (userExists(usernameToCheck)) {
+        res.send("username already exists");
+    }else {
+      let NEWuser = {
+          "username": `${inputValue.username}`,
+          "password": `${inputValue.password}`
+          }
+        
+          myObject.push(NEWuser);
+          // let newData2 = JSON.stringify(myObject);
+          let newData2 = JSON.stringify(myObject)
+          
+          fs.writeFile("./user.json", newData2, (err) => {
+          // Error checking
+          if (err) throw err;
+          });
+      
+        // Process the input value or save to database
+        res.send(`Welcome player!! ${inputValue.username}`)
+    }
+});
+
+app.post('/login', (req, res) => {
+    const inputValue = req.body
+    const usernameToCheck = inputValue.username;
+
+    if (userExists(usernameToCheck)) {
+      if( myObject.some(user => user.password == inputValue.password)){
+        res.send(`Welcome player!! ${inputValue.username}`);
+      }else{
+        res.send("worng password!!")
+      }
+    }else {
+        res.send("username don't exit");
+    }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
